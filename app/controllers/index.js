b6ecef348db6119b670ff8f35611ce6cfc348f09
Ricardo Magalhães <ricardomrfin@gmail.com>
2021-04-14 19:25:03 -0300

    controller de segurança adicionado

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,19 @@
 module.exports.index = function (application,req,res) {
     res.render('index');
 }
+
+module.exports.autenticar = function (application,req,res) {
+    var dadosForm = req.body;
+
+    req.assert('usuario','Campo usuário precisa ser preenchido, tente novamente').notEmpty();
+    req.assert('senha','Campo senha precisa ser preenchido, tente novamente').notEmpty()
+
+    var erros = req.validationErrors();
+
+    if (erros){
+        res.render('index',{validacao: erros});
+        return;
+    }
+
+    res.send('sessão criada com sucesso, parabéns.');
+}
